85e7953e8d52d5690d58c78929d4fdf7d49ea7b7
echollet <eric.chollet846@orange.fr>
2025-05-22 07:28:16 +0200

    ajout des données lnk_stop_point_line dans la base; .dump de la base (14 stop_points restant à renseigner dans lnk_stop_point_line)

--- get_pagination.py
+++ get_pagination.py
@@ -19,9 +19,13 @@ def get_pagination(url:str, token_base64:str)->int:
     json_data = x.json()
     #logging.info("get_pagination: json_data {}".format(json_data))
 
-    total_result = json_data["pagination"]["total_result"]
-    items_per_page = json_data["pagination"]["items_per_page"]
-
+    try:
+        total_result = json_data["pagination"]["total_result"]
+        items_per_page = json_data["pagination"]["items_per_page"]
+    except KeyError:
+        logging.error("url {} => No pagination.".format(url))
+        return 0
+    
     if items_per_page > 0 :
         nb_pages = int(total_result / items_per_page) 
         if total_result % items_per_page > 0:
